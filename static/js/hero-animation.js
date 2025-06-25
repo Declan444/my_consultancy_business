464322f1a295f94e6b57ed13ce4baf93f4bae21f
Declan <laresearchlabs@gmail.com>
2025-06-25 12:45:05 +0100

    change animation speed

--- static/js/hero-animation.js
+++ static/js/hero-animation.js
@@ -19,5 +19,5 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     updatePhrase();
-    setInterval(updatePhrase, 6000); // Change phrases every 8 seconds
+    setInterval(updatePhrase, 7000); // Change phrases every 8 seconds
 });
